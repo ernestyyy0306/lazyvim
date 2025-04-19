d3fc6675f9fe2fa6732b66ac4eabc1b0d7b1a94e
ernestyap <ernestyap@Ernest-Yaps-Mac.local>
2025-04-19 22:09:12 +0800

    feat: cspell integration

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,15 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local keymap = vim.keymap.set
+
+-- Fix Spell checking
+keymap("n", "z0", "1z=", {
+  desc = "Fix world under cursor",
+})
+
+keymap("n", "<leader>cb", "<cmd>lua require('utils.cspell').add_word_to_c_spell_dictionary()<CR>", {
+  noremap = true,
+  silent = true,
+  desc = "Add Word to Dictionary",
+})
