503f9f37061f3681c9aee9295f3a777f24b14df5
ernestyyy0306 <ernestyyy0306@gmail.com>
2026-03-18 23:29:52 +0800

    feat: copilot cmp

--- lua/plugins/copilot-cmp.lua
+++ lua/plugins/copilot-cmp.lua
@@ -1,4 +1,24 @@
 return {
-  "zbirenbaum/copilot-cmp",
-  enabled = false,
+  {
+    "zbirenbaum/copilot.lua",
+    config = function()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
+    end,
+  },
+  { "hrsh7th/nvim-cmp" },
+  {
+    "zbirenbaum/copilot-cmp",
+    dependencies = { "hrsh7th/nvim-cmp" },
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-cmdline" },
+  { "saadparwaiz1/cmp_luasnip" },
 }
