6d371c865e6bb265e7d55f83034bd60dbacbfba6
ernestyyy0306 <ernestyyy0306@gmail.com>
2026-03-20 18:51:51 +0800

    refactor: cleanup

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,33 +1,5 @@
-local Dashboard = require("utils.dashboard")
-
 return {
   "folke/snacks.nvim",
-  init = function()
-    vim.api.nvim_create_autocmd("VimEnter", {
-      callback = function()
-        local is_git_commit = vim.fn.argv(0):match("COMMIT_EDITMSG")
-
-        if not is_git_commit then
-          vim.schedule(function()
-            -- open explorer
-            require("snacks").explorer()
-            Dashboard.focus_snacks_dashboard()
-
-            -- refocus dashboard
-            for _, win in ipairs(vim.api.nvim_list_wins()) do
-              local buf = vim.api.nvim_win_get_buf(win)
-              local ft = vim.api.nvim_get_option_value("filetype", { buf = buf })
-
-              if ft == "snacks_dashboard" then
-                vim.api.nvim_set_current_win(win)
-                break
-              end
-            end
-          end)
-        end
-      end,
-    })
-  end,
   opts = {
     dashboard = {
       preset = {
